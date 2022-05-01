9eef8ea16b1cdda426a691867859e883ec192061
Silas Schnurr <silas@schnurr.one>
2022-05-01 14:47:10 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DebuggerComponent } from './ui-components/debugger/debugger.component';
+import { DesignerComponent } from './ui-components/designer/designer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DebuggerComponent,
+    DesignerComponent
   ],
   imports: [
     BrowserModule
