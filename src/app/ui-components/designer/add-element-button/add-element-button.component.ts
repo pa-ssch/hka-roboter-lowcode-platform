03228f4e14f71393fde55a702d2490396337c6d0
Silas Schnurr <silas@schnurr.one>
2022-05-18 00:17:27 +0200

    implement drag and drop

--- src/app/ui-components/designer/add-element-button/add-element-button.component.ts
+++ src/app/ui-components/designer/add-element-button/add-element-button.component.ts
@@ -16,4 +16,48 @@ export class AddElementButtonComponent {
   position: string;
   left: string;
   top: string;
+
+  private mousePosition: any;
+  private dragOffset: any;
+  private isDown: boolean;
+  private btn: any;
+
+  constructor() {
+    this.mousePosition = {
+      x: 0,
+      y: 0,
+    };
+    this.isDown = false;
+    this.dragOffset = [0, 0];
+  }
+  ngOnInit() {
+    this.btn = document.getElementsByClassName('add-button-div')[0];
+  }
+
+  mousedown($event: any) {
+    this.isDown = true;
+
+    $event.preventDefault();
+    this.dragOffset = [
+      this.btn.offsetLeft - $event.clientX,
+      this.btn.offsetTop - $event.clientY,
+    ];
+  }
+  mousemove($event: any) {
+    $event.preventDefault();
+
+    if (this.isDown) {
+      var mousePosition = {
+        x: $event.clientX,
+        y: $event.clientY,
+      };
+
+      this.btn.style.left = mousePosition.x + this.dragOffset[0] + 'px';
+      this.btn.style.top = mousePosition.y + this.dragOffset[1] + 'px';
+    }
+  }
+  mouseup($event: any) {
+    this.isDown = false;
+    $event.preventDefault();
+  }
 }
