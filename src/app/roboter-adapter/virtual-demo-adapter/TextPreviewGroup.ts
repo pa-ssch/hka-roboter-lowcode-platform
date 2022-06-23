2eb5f8e70a3f1530a664123d46af34f7788391fa
Silas Schnurr <git@schnurr.one>
2022-06-23 09:15:54 +0200

    implement preview text interpretation

--- src/app/roboter-adapter/virtual-demo-adapter/TextPreviewGroup.ts
+++ src/app/roboter-adapter/virtual-demo-adapter/TextPreviewGroup.ts
@@ -1,16 +1,55 @@
 import { PreviewGroupType } from '../adapter-definition/enums/preview-group-type.enum';
 import { IPreviewGroup } from '../adapter-definition/interfaces/preview/preview-group.interface';
+import { IRobotFunctinoalityArgument } from '../adapter-definition/interfaces/robot-functionality/robot-functionality-argument.interface';
 import { IRobotFunctionality } from '../adapter-definition/interfaces/robot-functionality/robot-functionality.interface';
 
 export class TextPreviewGroup implements IPreviewGroup {
   name: string;
   type: PreviewGroupType;
-  previewData: any;
+  previewData: TextPreviewData;
 
   constructor(workflow: IRobotFunctionality[], workflowNumber: number) {
     this.name = `Debugging Workflow ${workflowNumber}`;
     this.type = PreviewGroupType.textual;
-    // TODO: define a list of what comes in what order (for the debug arrow)
-    // define how what and how indented is written down
+    this.previewData = new TextPreviewData();
+
+    workflow.forEach((workflowElement) => {
+      workflowElement.getFunctionalityList().forEach((functionality) => {
+        if (functionality.identifier === 'on-startup') {
+          this.previewData.script.push(
+            new ScriptElement(0, functionality.parameterizedDisplayName)
+          );
+        } else if (
+          functionality.identifier === 'wait' ||
+          functionality.identifier === 'switch-light'
+        ) {
+          let displayName = this.stringFormat(
+            functionality.parameterizedDisplayName,
+            functionality.actualArgumentValues
+          );
+          this.previewData.script.push(new ScriptElement(1, displayName));
+        }
+      });
+    });
+
+    this.previewData.script.forEach((element: ScriptElement, index: number) => {
+      this.previewData.steps.push(index);
+    });
   }
+
+  stringFormat(text: string, args: IRobotFunctinoalityArgument[]) {
+    for (let i = 0; i < args.length; i++) {
+      text = text.replace('{' + i + '}', args[i].value);
+    }
+    return text;
+  }
+}
+
+class TextPreviewData {
+  script: ScriptElement[] = [];
+  steps: number[] = [];
+}
+
+class ScriptElement {
+  constructor(public intention: number, public text: string) {}
 }
